3d1147b61defeb4d3239d1f67a535e9c7b915ac4
quark1412 <hvphuongdts@gmail.com>
2023-10-04 10:29:18 +0700

    Add Bai1_5.cs

---
+++ Bai1_5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+public class Bai1_5
+{
+	public static void Main()
+	{
+		string path = Console.ReadLine();
+		
+		if (Directory.Exists(path))
+		{
+			string[] files = Directory.GetFileSystemEntries(path);
+
+			foreach (string file in files)
+			{
+				Console.WriteLine(file);
+			}
+		}
+		else
+		{
+			Console.WriteLine("Duong dan khong ton tai!");
+		}
+	}
+}
