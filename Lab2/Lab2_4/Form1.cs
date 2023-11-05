9c68c7b0812c2aec51bdffe76573001454750431
quark1412 <40156711+quark1412@users.noreply.github.com>
2023-11-05 15:30:39 +0700

    Update Lab2_4/Form1.cs

--- Lab2/Lab2_4/Form1.cs
+++ Lab2/Lab2_4/Form1.cs
@@ -16,24 +16,19 @@ namespace baitap24
 {
     public partial class Form1 : Form
     {
-        // Tạo một đối tượng Random
         Random random = new Random();
 
         public Form1()
         {
             InitializeComponent();
-            // Đăng ký phương thức xử lý sự kiện Paint cho Form
             this.Paint += new PaintEventHandler(Form1_Paint);
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            // Lấy đối tượng Graphics từ tham số PaintEventArgs
             Graphics g = e.Graphics;
-            // Sinh ra các số ngẫu nhiên cho tọa độ x, y của chuỗi "Paint Event"
             int x = random.Next(0, this.Width - 100);
             int y = random.Next(0, this.Height - 50);
-            // Vẽ chuỗi "Paint Event" tại vị trí ngẫu nhiên trên Form
             g.DrawString("Paint Event", new Font("Arial", 20), Brushes.Red, x, y);
         }
 
